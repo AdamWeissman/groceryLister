42cad6884eb1086da84a131eb16a6cc2c483e764
AdamWeissman <adam@comedytragedyepic.com>
2020-11-29 19:53:14 -0500

    mostly working but not for preexisting li elements

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const inputSelect = document.querySelector("input[name='newItem']")
 const mainList = document.querySelector('ul');
+const allListItems = document.querySelectorAll("li")
+
+for (let x = 0; x < allListItems; x++) {
+  allListItems[x].addEventListener("click", myList);
+}
+
+function myList(){
+  var temp = this.classList.toggle('red');
+  if (temp) {
+    let span = document.createElement('span');
+    span.textContent = 'x';
+    span.addEventListener('click', function() {
+      this.parentElement.remove()
+    })
+    this.appendChild(span);
+  }
+  else {
+    this.getElementsByTagName('span')[0].remove()
+  }
+}
+
 inputSelect.addEventListener('keypress', function(event){
-  if (event.keyCode === 13){ //13 is enter
+  if (event.keyCode === 13){ 
     console.log(event.keyCode);
     makeNew();
   } 
 
 })
-
-const allListItems = document.querySelectorAll('li');
-for (let x = 0; x < allListItems; x++) {
-  allListItems[x].addEventListener('click', myList);
-}
 
 function makeNew(){
   let li = document.createElement('li');
@@ -23,6 +39,3 @@ function makeNew(){
   mainList.appendChild(li);
 }
 
-function myList(){
-  var temp = this.classList.toggle('red')
-}
